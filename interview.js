98fe77683db8a394f79c04eed62409defc503e99
krishna <92731672+krishnaju07@users.noreply.github.com>
2024-07-21 15:18:50 +0530

    Update interview.js

--- interview.js
+++ interview.js
@@ -1,3 +1,6 @@
+//Event Loop
+// https://youtu.be/eiC58R16hb8?si=m1sPAgkRgwPxqKbA
+
 // 1 ---> Fizz Buzz Implementation
 
 // Qus : Given an integer n, for every integer i <= n, the task is to print “FizzBuzz” if i is divisible by 3 and 5, “Fizz” if i is divisible by 3, “Buzz” if i is divisible by 5 or i (as a string) if none of the conditions are true.
